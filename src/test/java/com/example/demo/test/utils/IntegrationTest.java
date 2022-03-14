1fa37a3de26083e4fe22baa2418577f3d5db848d
Godziątkowski Jan <godziatkowski.jan@gmail.com>
2022-03-14 18:59:37 +0100

    init project

---
+++ src/test/java/com/example/demo/test/utils/IntegrationTest.java
@@ -0,0 +1,20 @@
+package com.example.demo.test.utils;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@SpringBootTest
+@AutoConfigureMockMvc
+public @interface IntegrationTest {
+}
